a3525134c174bad7d5c8ae7ec08f922cc75c8e96
Henrique Dias Campos <henriquediascampos01@gmail.com>
2019-02-17 16:10:54 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { appRoutingModule } from './app-routing.module';
+import { TarefasModule } from './tarefas';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    appRoutingModule,
+    TarefasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
